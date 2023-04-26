00d20d36fcca9391b13419224562ff1699a6608f
yeahboi21 <99054994+yeahboi21@users.noreply.github.com>
2023-04-26 13:13:06 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,24 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    Action = receivedNumber
+})
+let Action = 0
 joystickbit.initJoystickBit()
 radio.setGroup(17)
 basic.showIcon(IconNames.Skull)
 basic.forever(function () {
-	
+    if (joystickbit.getButton(joystickbit.JoystickBitPin.P12)) {
+        radio.sendNumber(5)
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P13)) {
+        radio.sendNumber(6)
+    } else {
+    	
+    }
+})
+basic.forever(function () {
+    if (Action == 5) {
+        wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 60)
+    }
+    if (Action == 6) {
+        wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
+    }
 })
